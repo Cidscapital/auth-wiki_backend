62b4408f4e0a2f939325e10cb0f6f2d2f617b765
David <daviddanielcareer@gmail.com>
2022-07-27 04:00:30 +0100

    updated the admin.py file and saved the forms.py file

--- library/forms.py
+++ library/forms.py
@@ -2,13 +2,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+#we will import the name of whatever model to be created. Custom user was just a sample
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        #model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('',)
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
+        #model = CustomUser
         fields = UserChangeForm.Meta.fields
