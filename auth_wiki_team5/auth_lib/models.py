8ac074be0aaa9387df367c14cb116e94c3d44e3c
chassis <38101674+Code-Ebullient@users.noreply.github.com>
2022-07-28 00:11:49 +0100

    updated app model

--- auth_wiki_team5/auth_lib/models.py
+++ auth_wiki_team5/auth_lib/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User(models.Model):
+    full_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = "user"
+
+    def __str__(self) -> str:
+        return self.full_name
